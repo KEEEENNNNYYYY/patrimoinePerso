bfd1e87b2dc5c3582018014c7e3078308f69fb86
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-12-12 16:21:20 +0100

    Add port configuration for Render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,    // Permet à l'application d'être accessible sur tous les hôtes
+    port: 5173     // Spécification du port sur lequel l'application écoute
+  }
+});
