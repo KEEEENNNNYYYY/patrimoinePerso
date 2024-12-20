9f6645de54370d9f61998a7aae4d425af9e603ab
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-12-20 18:19:40 +0100

    feat : new route for login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,30 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Correction des importations
+import Login from './component/login'; // Assurez-vous que le chemin vers Login est correct
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router> {/* Enveloppez votre application avec Router */}
+      <Routes> {/* Définissez les routes de votre application */}
+        <Route path="/login" element={<Login />} /> {/* Définissez la route /login pour afficher la page Login */}
+      </Routes>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="/assets/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
-      
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
