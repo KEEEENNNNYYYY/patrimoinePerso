61e17d4ea5b33bdc7e701b5c6ce81521e70388f4
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-12-26 11:35:28 +0100

    test : deploy test 9

--- back-end/firebase-admin.js
+++ back-end/firebase-admin.js
@@ -12,7 +12,21 @@
 // });
 // module.exports = admin;
 const admin = require('firebase-admin');
-const serviceAccount = require('./mypatrimoineapi-firebase-adminsdk-xmpls-ab41553080.json'); // Charge les informations Firebase
+require('dotenv').config(); // Charge les variables d'environnement
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Remplace les \\n par \n pour la clé privée
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  clientId: process.env.FIREBASE_CLIENT_ID,
+  authUri: process.env.FIREBASE_AUTH_URI,
+  tokenUri: process.env.FIREBASE_TOKEN_URI,
+  authProviderCertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  clientCertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+  universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+}; // Charge les informations Firebase
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
