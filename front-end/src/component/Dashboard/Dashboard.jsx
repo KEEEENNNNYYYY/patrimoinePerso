8b1d386b3217861cd06f2a32dcb396107f175298
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-01-14 15:42:16 +0100

    feat : new component

--- front-end/src/component/Dashboard/Dashboard.jsx
+++ front-end/src/component/Dashboard/Dashboard.jsx
@@ -2,47 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { auth } from '../../firebase';
 import { signOut } from 'firebase/auth';
 import Axios from 'axios';
+import Navigationbar from '../navbar/navbar';
+
 
 const Dashboard = () => {
-    const [user, setUser] = useState(null); // Initialiser à null
-    const [error, setError] = useState(null);
+    
 
-    useEffect(() => {
-        // Récupérer les données de l'utilisateur
-        const fetchUser = async () => {
-            try {
-                const response = await Axios.get('https://patrimoineperso-backend.onrender.com/users');
-                if (response.data && response.data.length > 0) {
-                    setUser(response.data[0].first_name); // Utiliser first_name
-                } else {
-                    setError('Aucun utilisateur trouvé');
-                }
-            } catch (err) {
-                setError('Erreur de récupération de l\'utilisateur');
-                console.error(err);
-            }
-        };
-
-        fetchUser(); // Appeler la fonction pour récupérer les données
-    }, []); // L'effet s'exécute une seule fois au montage du composant
-
-    const handleLogout = async () => {
-        try {
-            await auth.signOut(); // Déconnexion
-            window.location.replace('/login'); // Redirection vers la page de connexion
-        } catch (err) {
-            console.error("Erreur de déconnexion :", err.message);
-        }
-    };
+    
 
     return (
         <>
-            <div>
-                <button onClick={handleLogout}>Se déconnecter</button>
-                {error ? <p>{error}</p> : <p>{user ? `Bonjour, ${user}` : 'Chargement...'}</p>}
-            </div>
+            <Navigationbar /> 
+
         </>
     );
+
 };
 
 export default Dashboard;
