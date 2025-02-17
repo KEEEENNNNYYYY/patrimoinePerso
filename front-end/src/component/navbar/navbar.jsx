4dc41c66c4e02d502d7180464679ada905202cf9
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-02-17 08:06:53 +0100

    feat : added navbar

--- front-end/src/component/navbar/navbar.jsx
+++ front-end/src/component/navbar/navbar.jsx
@@ -1,91 +1,33 @@
-import React, { useState, useEffect } from 'react';
 import { auth } from '../../firebase';
-import { signOut } from 'firebase/auth';
-import Axios from 'axios';
-import './navbar.css';
-import './mobileNavbar.css'
+import { Admin , Resource , List } from 'react-admin';
+import {profilData} from "../../model/dataProvider.js";
 
 const Navigationbar = () => {
-    const [clicked, isClicked] = useState(false);
-    const [display, setDisplay] = useState('none');
 
-    function handleDisplay() {
-        if (clicked === true) {
-            setDisplay('flex');
-        }
-        else {
-            setDisplay('none');
-        }
-    }
-
-
-    // const [user, setUser] = useState(null); // Initialiser à null
-    // const [error, setError] = useState(null);
-
-    // useEffect(() => {
-    //     // Récupérer les données de l'utilisateur
-    //     const fetchUser = async () => {
-    //         try {
-    //             const response = await Axios.get('https://patrimoineperso-backend.onrender.com/users');
-    //             if (response.data && response.data.length > 0) {
-    //                 setUser(response.data[0].first_name); // Utiliser first_name
-    //             } else {
-    //                 setError('Aucun utilisateur trouvé');
-    //             }
-    //         } catch (err) {
-    //             setError('Erreur de récupération de l\'utilisateur');
-    //             console.error(err);
-    //         }
-    //     };
-
-    //     fetchUser(); 
-    // }, []); 
     const handleLogout = async () => {
         try {
             await auth.signOut(); // Déconnexion
-            window.location.replace('/login'); // Redirection vers la page de connexion
+            window.location.replace('/login');
         } catch (err) {
             console.error("Erreur de déconnexion :", err.message);
         }
     };
+    const UserList = () => (
+        <List>
+            <button onClick={handleLogout}>Disconnect</button>
+        </List>
+    );
+
     return (
         <>
-            <nav>
-                {/* {error ? <p>{error}</p> : <p>{user ? `User : ${user}` : 'Chargement...'}</p>} */}
-                <h1>
-                    MyPatrimoine
-                </h1>
-                <input type="text" />
-                <div className='right-container'>
-                    
-                    <button onClick={() => { isClicked(!clicked); handleDisplay(); }} className='setting-button'>
-                        <img
-                            className='nav-settings'
-                            src="../../../public/settings.png" alt="" />
-
-                    </button>
-                </div>
-
-                <div className='drop-down-menu-container' style={{ display: display }}>
-                    <button>
-                        Profil
-                    </button>
-                    <button>
-                        History
-                    </button>
-                    <button>
-                        Contact us
-                    </button>
-                    <button>
-                        Settings
-                    </button>
-                    <button onClick={handleLogout}>Disconnect</button>
-
-                </div>
-
-            </nav>
+            <Admin dataProvider={profilData} >
+                <Resource name="dashboard" list={UserList}/>
+                <Resource name="account" list={UserList}/>
+                <Resource name="settings" list={UserList}/>
+                <Resource name="disconnect" list={UserList} onClick={handleLogout}/>
+                <button onClick={handleLogout}>Disconnect</button>
+            </Admin>
         </>
-
     )
 }
 
